ced6921d824c22ea494a0da48acaa3dd42379200
Advansoftware <brunoantunes94@hotmail.com>
2024-05-14 20:58:34 -0300

    feat: add search initial

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
+import Search from "./search";
 
 const Header = () => {
   return (
     <div className="flex justify-between pt-6">
-      <Image src="./Logo.png" alt="Logo" height={30} width={100} />
+      <Image src="/Logo.png" alt="Logo" height={30} width={100} />
       <Button
         size="icon"
         variant="outline"
